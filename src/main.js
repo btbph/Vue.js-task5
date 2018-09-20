ebadd3bacf6d293597292ff3c79a8edec77e5998
Mikhail Ulyanov <m.ulyanov96@mail.ru>
2018-09-20 15:16:50 +0300

    add form product form

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(VeeValidate)
 
 new Vue({
   store,
--- src/views/admin/New.vue
+++ src/views/admin/New.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>
-    <div class="title">
-      <h1>This is Admin/New</h1>
-    </div>
-  </div>
+  <product-form
+    @save-product="addProduct"
+    :model="model"
+    :manufacturers="manufacturers">
+  </product-form>
 </template>
 
-<style></style>
+<script>
+  import ProductFrom from '@/components/products/ProductForm'
+  export default {
+    data () {
+      return {
+        model: {},
+        manufacturers: [
+          {
+            _id: 'sam',
+            name: 'Samsung',
+          },
+          {
+            _id: 'apple',
+            name: 'Apple',
+          },
+        ],
+      }
+    },
+    methods: {
+      addProduct (model) {
+        console.log('model', model)
+      }
+    },
+    components: {
+      'product-form': ProductFrom
+    }
+  }
+</script>
